d79311dc968a5ba134eb1312759606ba029bac0a
Brandon Braun <brandonbraun653@gmail.com>
2018-04-01 17:33:22 -0500

    update include formatting

--- IMU/LSM9DS1/LSM9DS1.hpp
+++ IMU/LSM9DS1/LSM9DS1.hpp
@@ -10,10 +10,10 @@
 #include "LSM9DS1_Registers.h"
 
 /* Thor Includes */
-#include "include/thor_config.h"
-#include "include/thor_definitions.h"
-#include "include/gpio.h"
-#include "include/spi.h"
+#include "Thor/include/thor_config.h"
+#include "Thor/include/thor_definitions.h"
+#include "Thor/include/gpio.h"
+#include "Thor/include/spi.h"
 
 #ifdef USING_FREERTOS
 #include "FreeRTOS.h"
